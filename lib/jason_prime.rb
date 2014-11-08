22734572b036ac5e344b9f45e4c9cc15ae64584c
Jason Smith <qqmoreimo@gmail.com>
2014-11-08 14:29:40 -0800

    Used Sieve of Erasthonese Algorithm   - JasonPrime#up_to() now works

--- lib/jason_prime.rb
+++ lib/jason_prime.rb
@@ -1,32 +1,20 @@
+require "pry"
+
 class JasonPrime
-  def initialize
-  end
-
-  # # returns prime numbers up to the the provided upper_limit
   def up_to(upper_limit=100)
-    potential_prime = 2
-    prime_numbers = [potential_prime]
+    integers_up_to_limit = (2..upper_limit).to_a
+    divisor_index = 0
 
-    while potential_prime < upper_limit
-      potential_prime += 1
-
-      prime_numbers.each do |prime|
-        print "Potential Prime: #{potential_prime} - Current Prime: #{prime}"
-        print " - Current Prime Numbers: #{prime_numbers.inspect}"
-        puts
-        if potential_prime % prime == 0
-          break
-        else
-          prime_numbers << potential_prime if !prime_numbers.include?(potential_prime)
-        end
+    while Math.sqrt(integers_up_to_limit.last) >= integers_up_to_limit[divisor_index]
+      prime_number_divisor = integers_up_to_limit[divisor_index]
+      integers_up_to_limit.select! do |integer|
+        integer % prime_number_divisor != 0 or integer == prime_number_divisor
       end
+      divisor_index += 1
     end
 
-    prime_numbers
+    integers_up_to_limit
   end
-
-  # # returns the first n prime numbers
-  # def generate(n)
-
-  # end
 end
+
+JasonPrime.new.up_to(10)
