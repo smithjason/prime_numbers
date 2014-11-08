9398d23893975e45f7e64d3ea4434d271dd4c559
Jason Smith <qqmoreimo@gmail.com>
2014-11-08 15:18:00 -0800

    Refactored Optimizations for JasonPrime#up_to into helper methods   - TODO: I feel like it would be better to implmeent a JasonPrime#generate(Nth_prime) method.  Will tinker for a bit in irb.

--- lib/jason_prime.rb
+++ lib/jason_prime.rb
@@ -1,16 +1,15 @@
 class JasonPrime
   def up_to(upper_limit=100)
     return nil if upper_limit < 2
-    integers_up_to_limit = (3..upper_limit).to_a
-    integers_up_to_limit.select!{ |num| num.odd? }.unshift(2)
-    primes!(integers_up_to_limit)
+    optimized_integers = optimize_integer_array((2..upper_limit).to_a)
+    primes(optimized_integers)
   end
 
   private
 
-  def primes!(integer_array)
+  def primes(integer_array)
     divisor_index = 0
-    while Math.sqrt(integer_array.last) >= integer_array[divisor_index]
+    while integer_less_than_square_root_of_limit(integer_array[divisor_index], integer_array.last)
       prime_number_divisor = integer_array[divisor_index]
       integer_array.select! do |integer|
         integer % prime_number_divisor != 0 or integer == prime_number_divisor
@@ -18,5 +17,13 @@ class JasonPrime
       divisor_index += 1
     end
     integer_array
+  end
+
+  def optimize_integer_array(integer_array)
+    integer_array.select!{ |num| num.odd? or num == 2}
+  end
+
+  def integer_less_than_square_root_of_limit(integer, upper_limit)
+    Math.sqrt(upper_limit) >= integer
   end
 end
