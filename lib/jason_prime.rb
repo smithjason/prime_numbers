ef153359239eef9c5f5d18cda6e27c0e1980ec5a
Jason Smith <qqmoreimo@gmail.com>
2014-11-08 14:00:52 -0800

    Got a semi-working Prime Class  - Need to actually get it to work

--- lib/jason_prime.rb
+++ lib/jason_prime.rb
@@ -1,3 +1,32 @@
 class JasonPrime
+  def initialize
+  end
 
+  # # returns prime numbers up to the the provided upper_limit
+  def up_to(upper_limit=100)
+    potential_prime = 2
+    prime_numbers = [potential_prime]
+
+    while potential_prime < upper_limit
+      potential_prime += 1
+
+      prime_numbers.each do |prime|
+        print "Potential Prime: #{potential_prime} - Current Prime: #{prime}"
+        print " - Current Prime Numbers: #{prime_numbers.inspect}"
+        puts
+        if potential_prime % prime == 0
+          break
+        else
+          prime_numbers << potential_prime if !prime_numbers.include?(potential_prime)
+        end
+      end
+    end
+
+    prime_numbers
+  end
+
+  # # returns the first n prime numbers
+  # def generate(n)
+
+  # end
 end
--- spec/lib/jason_prime_spec.rb
+++ spec/lib/jason_prime_spec.rb
@@ -3,9 +3,22 @@ require "jason_prime"
 
 describe JasonPrime do
   describe "initialize" do
-    it "should not be null" do
+    it "should not be nil" do
       myPrime = JasonPrime.new
       expect(myPrime).not_to be_nil
     end
   end
+
+  describe "up_to" do
+    it "returns no numbers when limit is less than 2"
+    it "returns 2 when limit is 2"
+    it "returns 2,3,5,7 when limit is 10"
+  end
+
+  # describe "generate" do
+  #   it "returns no numbers when n is 0"
+  #   it "returns 1 number when n is 1"
+  #   it "returns 3 numbers when n is 3"
+  #   it "returns 10 numbers when n is 10"
+  # end
 end
