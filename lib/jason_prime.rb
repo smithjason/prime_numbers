63d42080bf168903160bb04ec191216fcd1d6549
Jason Smith <qqmoreimo@gmail.com>
2014-11-08 15:03:51 -0800

    Updated JasoPrime#up_to   - Instead of sending whole array of ints from 2 to upper_limit, #up_to now only sends #primes! a list of odd integers from 2 to the upper_limit.

--- lib/jason_prime.rb
+++ lib/jason_prime.rb
@@ -1,18 +1,22 @@
 class JasonPrime
   def up_to(upper_limit=100)
     return nil if upper_limit < 2
+    integers_up_to_limit = (3..upper_limit).to_a
+    integers_up_to_limit.select!{ |num| num.odd? }.unshift(2)
+    primes!(integers_up_to_limit)
+  end
 
-    integers_up_to_limit = (2..upper_limit).to_a
+  private
+
+  def primes!(integer_array)
     divisor_index = 0
-
-    while Math.sqrt(integers_up_to_limit.last) >= integers_up_to_limit[divisor_index]
-      prime_number_divisor = integers_up_to_limit[divisor_index]
-      integers_up_to_limit.select! do |integer|
+    while Math.sqrt(integer_array.last) >= integer_array[divisor_index]
+      prime_number_divisor = integer_array[divisor_index]
+      integer_array.select! do |integer|
         integer % prime_number_divisor != 0 or integer == prime_number_divisor
       end
       divisor_index += 1
     end
-
-    integers_up_to_limit
+    integer_array
   end
 end
