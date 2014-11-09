383982f516a5993ed73c64a6070b93fe25d9a442
Jason Smith <qqmoreimo@gmail.com>
2014-11-08 18:57:37 -0800

    Changed usage to allow for input for the Nth prime the user wants to set as the max.  - i.e. 'ruby challenge.rb 10' prints out a 10x10 multiplication table

--- challenge.rb
+++ challenge.rb
@@ -4,7 +4,9 @@ def formatted_print(item)
   printf("%5d", "#{item}")
 end
 
-first_ten_primes = JasonPrime.new.primes_to_nth_prime(10)
+nth_prime = ARGV[0].to_i
+
+first_ten_primes = JasonPrime.new.primes_to_nth_prime(nth_prime)
 
 print "     "
 first_ten_primes.each { |prime| formatted_print(prime) }
