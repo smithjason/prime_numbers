c4a3436564479ae3dfa508abcdd326a7b2b7690f
Jason Smith <qqmoreimo@gmail.com>
2014-11-10 09:37:28 -0800

    Refactored a bit before sending.

--- challenge.rb
+++ challenge.rb
@@ -4,13 +4,12 @@ def formatted_print(item)
   printf("%5d", "#{item}")
 end
 
-ARGV.empty? ? nth_prime = 10 : nth_prime = ARGV[0].to_i
+nth_prime = 10
 
 first_ten_primes = JasonPrime.new.primes_to_nth_prime(nth_prime)
 
 print "     "
 first_ten_primes.each { |prime| formatted_print(prime) }
-
 puts
 
 first_ten_primes.each do |prime|
--- lib/jason_prime.rb
+++ lib/jason_prime.rb
@@ -1,19 +1,20 @@
 class JasonPrime
-  @@BASE_PRIMES = [2,3]
+  BASE_PRIMES = [2,3]
 
   def primes_to_upper_limit(upper_limit=100)
-    return nil if upper_limit < 2
-    return @@BASE_PRIMES.take_while { |prime| prime <= upper_limit } if upper_limit <= 3
+    return nil if upper_limit < BASE_PRIMES.first
+    return BASE_PRIMES.take_while{ |prime| prime <= upper_limit } if upper_limit <= BASE_PRIMES.last
 
-    optimized_integers = optimize_integer_array((2..upper_limit).to_a)
+    list_of_integers = Array(BASE_PRIMES.first..upper_limit)
+    optimized_integers = get_all_odds_and_first_base_prime(list_of_integers)
     primes_in_integer_array(optimized_integers)
   end
 
   def primes_to_nth_prime(nth_prime)
     return nil if nth_prime < 1
-    return @@BASE_PRIMES.take(nth_prime) if nth_prime <= 2
+    return BASE_PRIMES.take(nth_prime) if nth_prime <= 2
 
-    prime_numbers = @@BASE_PRIMES.dup
+    prime_numbers = BASE_PRIMES.dup
     potential_prime = prime_numbers.last
 
     while prime_numbers.length < nth_prime
@@ -21,7 +22,7 @@ class JasonPrime
       prime = true
 
       prime_numbers.each do |prime_number|
-        break if integer_is_less_than_or_equal_to_square_root_of_limit?(potential_prime, prime_number)
+        break if less_than_square_root_of_limit?(potential_prime, prime_number)
         prime = false if is_composite_integer?(potential_prime, prime_number)
         break if !prime
       end
@@ -37,7 +38,7 @@ class JasonPrime
   def primes_in_integer_array(integer_array)
     divisor_index = 0
 
-    while integer_is_less_than_or_equal_to_square_root_of_limit?(integer_array[divisor_index], integer_array.last)
+    while less_than_square_root_of_limit?(integer_array[divisor_index], integer_array.last)
       prime_number_divisor = integer_array[divisor_index]
 
       integer_array.select! do |integer|
@@ -54,11 +55,11 @@ class JasonPrime
     integer % divisor == 0
   end
 
-  def optimize_integer_array(integer_array)
-    integer_array.select!{ |num| num.odd? or num == 2}
+  def get_all_odds_and_first_base_prime(integer_array)
+    integer_array.select!{ |num| num.odd? || num == BASE_PRIMES.first }
   end
 
-  def integer_is_less_than_or_equal_to_square_root_of_limit?(integer, limit)
+  def less_than_square_root_of_limit?(integer, limit)
     Math.sqrt(limit) >= integer
   end
 end
