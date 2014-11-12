a6d27fe15e4f077a807d438352ea22734c323a31
Jason Smith <qqmoreimo@gmail.com>
2014-11-12 09:59:24 -0800

    Added PrimePrinter Class   - Handles output of primes   - Result of refactoring challenge.rb Refactored PrimeHelper Class   - Removed #primes_to_upper_limit as it was not being utilized   - Removed un-used helper methods

--- challenge.rb
+++ challenge.rb
@@ -1,20 +1,8 @@
-require_relative "lib/jason_prime"
-
-def formatted_print(item)
-  printf("%5d", "#{item}")
-end
+require_relative "helpers/prime_helper"
+require_relative "helpers/prime_printer"
 
 nth_prime = 10
 
-first_ten_primes = JasonPrime.new.primes_to_nth_prime(nth_prime)
-
-print "     "
-first_ten_primes.each { |prime| formatted_print(prime) }
-puts
-
-first_ten_primes.each do |prime|
-  formatted_print(prime)
-  first_ten_primes.each { |multiplier| formatted_print(prime * multiplier) }
-  puts
-end
+first_ten_primes = PrimeHelper.primes_to_nth_prime(nth_prime)
+PrimePrinter.print_multiplication_table(first_ten_primes)
 
